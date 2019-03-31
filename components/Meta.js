987997a8cce74aa5635ede726bccd9ff3bdf8e7d
John Alexander Agudelo Galeano <john@synapbox.com>
2019-03-30 22:04:20 -0600

    feat: component Meta inject header html with link css

---
+++ components/Meta.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="virwport" content="width=divice-width,initial-scale=1" />
+    <meta chartset="utf-8" />
+    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <title>sick Fits!</title>
+  </Head>
+)
+
+export default Meta;
